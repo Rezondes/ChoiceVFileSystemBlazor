af2d9d4f866473cde1600eab404432b04df7e2eb
Rezondes <stevenrichter1902@gmail.com>
2024-09-13 10:02:49 +0200

    fix(auth): remove hsts

--- ChoiceVFileSystemBlazor/Program.cs
+++ ChoiceVFileSystemBlazor/Program.cs
@@ -231,13 +231,7 @@ builder.Services.AddSignalR();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler(Error.GetRedirectUrl(), createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
+app.UseExceptionHandler(Error.GetRedirectUrl(), createScopeForErrors: true);
 
 app.UseHttpsRedirection();
 
