17f903b6f07e4a830675a11457719c87ecf0e4f7
Rezondes <stevenrichter1902@gmail.com>
2024-09-22 18:21:37 +0200

    feat(bankaccount): add BankAccountOverviewDataGrid.razor to CharacterView.razor & filled BankAccountView.razor with first data

--- ChoiceVRefitClient/IBankAccountApi.cs
+++ ChoiceVRefitClient/IBankAccountApi.cs
@@ -8,4 +8,10 @@ public interface IBankAccountApi
 {
     [Get("/api/v1/bankaccount")]
     Task<ApiResponse<List<BankAccountApiModel>>> GetAllAsync(); 
+    
+    [Get("/api/v1/bankaccount?bankaccountId={bankaccountId}")]
+    Task<ApiResponse<BankAccountApiModel?>> GetBankAccountByIdAsync(int bankaccountId); 
+    
+    [Get("/api/v1/bankaccount?characterId={characterId}")]
+    Task<ApiResponse<List<BankAccountApiModel>>>  GetBankAccountsByCharacterIdAsync(int characterId); 
 }
