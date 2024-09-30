512d7f54880ad6c6584b0750db39908df7315de3
Rezondes <stevenrichter1902@gmail.com>
2024-09-30 19:11:48 +0200

    feat(rank): add RankEnumExtensions-Class

--- ChoiceVFileSystemBlazor/Database/_Shared/RankEnum.cs
+++ ChoiceVFileSystemBlazor/Database/_Shared/RankEnum.cs
@@ -9,3 +9,20 @@ public enum RankEnum
     Rank4,
     Admin
 }
+
+public static class RankEnumExtensions
+{
+    public static string GetDisplayText(this RankEnum rank)
+    {
+        return rank switch
+        {
+            RankEnum.None => "Kein Zugriff",
+            RankEnum.Rank1 => "Support-Trainee",
+            RankEnum.Rank2 => "Supporter",
+            RankEnum.Rank3 => "Senior Supporter",
+            RankEnum.Rank4 => "Head of Support",
+            RankEnum.Admin => "Administrator",
+            _ => throw new ArgumentOutOfRangeException(nameof(rank), rank, null)
+        };
+    }
+}
