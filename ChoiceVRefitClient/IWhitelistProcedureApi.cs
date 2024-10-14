7e6704c01aa96ecedc99697766f2e8375ba28538
Rezondes <stevenrichter1902@gmail.com>
2024-10-14 14:48:46 +0200

    feat(whitelist): added block-procedure button

--- ChoiceVRefitClient/IWhitelistProcedureApi.cs
+++ ChoiceVRefitClient/IWhitelistProcedureApi.cs
@@ -10,4 +10,8 @@ public interface IWhitelistProcedureApi
     
     [Get("/api/v1/procedure?procedureId={procedureId}")]
     Task<ApiResponse<WhitelistProcedureApiModel?>> GetByProcedureIdAsync(int procedureId); 
+    
+    [Put("/api/v1/procedure/blocked?procedureId={procedureId}&state={state}")]
+    [Headers("Authorization: Bearer")]
+    Task<ApiResponse<string>> SetBlockedStateAsync(int procedureId, bool state); 
 }
