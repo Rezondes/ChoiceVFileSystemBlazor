45732e4ba1e8e0bb950615e587838fbe42bc36eb
Rezondes <stevenrichter1902@gmail.com>
2024-11-12 21:34:16 +0100

    version: v12112024_1

--- ChoiceVFileSystemBlazor/Constants.cs
+++ ChoiceVFileSystemBlazor/Constants.cs
@@ -2,7 +2,7 @@
 
 public static class Constants
 {
-    public const string VERSION = "v08112024_3";
+    public const string VERSION = "v12112024_1";
     
     public const int TOOLTIP_DURATION = 500;
     public const string CLOUD_URL = "https://cloud.choicev.net/apps/dashboard/";
