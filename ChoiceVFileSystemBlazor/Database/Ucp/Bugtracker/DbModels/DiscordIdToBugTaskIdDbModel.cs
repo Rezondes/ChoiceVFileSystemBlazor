014440eb2ca53a267e46178865256f39a543420f
Rezondes <stevenrichter1902@gmail.com>
2024-11-16 14:52:39 +0100

    feat(bugtracker): added DiscordIdToBugTaskIdDbModelExtensions

--- ChoiceVFileSystemBlazor/Database/Ucp/Bugtracker/DbModels/DiscordIdToBugTaskIdDbModel.cs
+++ ChoiceVFileSystemBlazor/Database/Ucp/Bugtracker/DbModels/DiscordIdToBugTaskIdDbModel.cs
@@ -1,3 +1,6 @@
+using ChoiceVFileSystemBlazor.Services.Vikunja;
+using ChoiceVFileSystemBlazor.Services.Vikunja.Models;
+
 namespace ChoiceVFileSystemBlazor.Database.Ucp.Bugtracker.DbModels;
 
 public class DiscordIdToBugTaskIdDbModel
@@ -18,4 +21,48 @@ public class DiscordIdToBugTaskIdDbModel
     public int BugTaskId { get; set; }
     
     public string BugTaskName { get; set; }
+}
+
+public enum BugTaskStatus
+{
+    Pending,
+    Rejected,
+    Accepted,
+    Done
+}
+
+public static class DiscordIdToBugTaskIdDbModelExtensions
+{
+    public static async Task<BugTaskStatus> GetStatus(this DiscordIdToBugTaskIdDbModel dbTask, 
+        VikunjaClientService vikunjaClientService, List<VikunjaTask> allScpBugs, List<VikunjaTask> allChoiceVBugs)
+    {
+        if (allScpBugs.Select(x => x.Id).Contains(dbTask.BugTaskId))
+            return BugTaskStatus.Pending;
+
+        if (!allChoiceVBugs.Select(x => x.Id).Contains(dbTask.BugTaskId)) 
+            return BugTaskStatus.Rejected;
+        
+        var result = await vikunjaClientService.Client.HandleApiRequestAsync(
+            async _ => await vikunjaClientService.Client.GetTaskByIdAsync(dbTask.BugTaskId));
+
+        return result.Data!.Done ? BugTaskStatus.Done : BugTaskStatus.Accepted;
+
+    }
+    
+    public static async Task<BugTaskStatus> GetStatus(this DiscordIdToBugTaskIdDbModel dbTask, VikunjaClientService vikunjaClientService)
+    {
+        var allScpBugs = await vikunjaClientService.Client.GetAllTasksInProjectAsync(vikunjaClientService.ScpBugsProjectId);
+        if (allScpBugs.Select(x => x.Id).Contains(dbTask.BugTaskId))
+            return BugTaskStatus.Pending;
+
+        var allChoiceVBugs = await vikunjaClientService.Client.GetAllTasksInProjectAsync(vikunjaClientService.ChoiceVBugsProjectId);
+        if (!allChoiceVBugs.Select(x => x.Id).Contains(dbTask.BugTaskId)) 
+            return BugTaskStatus.Rejected;
+        
+        var result = await vikunjaClientService.Client.HandleApiRequestAsync(
+            async _ => await vikunjaClientService.Client.GetTaskByIdAsync(dbTask.BugTaskId));
+
+        return result.Data!.Done ? BugTaskStatus.Done : BugTaskStatus.Accepted;
+
+    }
 }
